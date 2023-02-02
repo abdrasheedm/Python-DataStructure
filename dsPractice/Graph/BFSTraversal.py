55c0f3af8debd3f8cc00e4e9b65e78b68ee6848b
abdrasheedm <rasheedvaniyannurrashi175@gmail.com>
2023-02-02 11:31:46 +0530

    BFS and DFS in graph

--- dsPractice/Graph/BFSTraversal.py
+++ dsPractice/Graph/BFSTraversal.py
@@ -1,6 +1,34 @@
-lksdljflksdflkdsjfljj
-kodsjflkjsdfjsdf
-sakjflksdfjas
-askdfjaslkdjf
-askdjfas
-aksdjfkljsdfjkl
+class Graph:
+    def __init__(self, gdict=None):
+        if gdict is None:
+            gdict = {}
+        self.gdict = gdict
+
+    def add_edge(self, vertex, edge):
+        self.gdict[vertex].append(edge)
+
+    def bfs(self, vertex):
+        visited = [vertex]
+        queue = [vertex]
+        while queue:
+            de_vertext = queue.pop(0)
+            print(de_vertext)
+            for adjacent_vertex in self.gdict[de_vertext]:
+                if adjacent_vertex not in visited:
+                    queue.append(adjacent_vertex)
+                    visited.append(adjacent_vertex)
+
+
+costomDict = {
+    "a": ["b", "c"],
+    "b": ["a", "d", "e"],
+    "c": ["a", "e"],
+    "d": ["b", "e", "f"],
+    "e": ["d", "f", "c"],
+    "f": ["d", "e"]
+}
+
+graph = Graph(costomDict)
+graph.add_edge("a", "f")
+print(graph.gdict)
+graph.bfs("a")
---
+++ dsPractice/Graph/DFSTrasversal.py
@@ -0,0 +1,39 @@
+class Graph:
+    def __init__(self,  gdict=None):
+        if gdict is None:
+            gdict = {}
+        self.gdict = gdict
+
+    def add_edge(self, vertex, edge):
+        if vertex in self.gdict and edge in self.gdict:
+            self.gdict[vertex].append(edge)
+        else:
+            print("there no vertex")
+
+
+    def dfs(self, vertex):
+        visited = [vertex]
+        stack = [vertex]
+        while stack:
+            pop_vertex = stack.pop()
+            print(pop_vertex)
+            for adjacent_vertex in self.gdict[pop_vertex]:
+                if adjacent_vertex not in visited:
+                    visited.append(adjacent_vertex)
+                    stack.append(adjacent_vertex)
+
+
+
+costomDict = {
+    "a": ["b", "c"],
+    "b": ["a", "d", "e"],
+    "c": ["a", "e"],
+    "d": ["b", "e", "f"],
+    "e": ["d", "f", "c"],
+    "f": ["d", "e"]
+}
+
+graph = Graph(costomDict)
+graph.add_edge("a", "g")
+print(graph.gdict)
+graph.dfs("a")
